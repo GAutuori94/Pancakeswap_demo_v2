a48a07b6c100248689058c4f24a68f2c91f88382
Andrea Barberio <99880853+AndreaBarberio@users.noreply.github.com>
2022-09-18 17:54:59 +0200

    Update: Fixed import issues adding type, dropDown hook initialized

--- src/components/shared/Nav.tsx
+++ src/components/shared/Nav.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import styles from '../../style/Nav.module.css';
+import '../../style/Nav.css';
 import { Link } from 'react-router-dom';
 import * as data from './links.json';
-
+import pancakeWritten from '../../assets/images/svg/utilities/pancakeWritten.svg';
 import logoPancake from '../../assets/images/svg/utilities/logoPancake.svg';
 import logoNET from '../../assets/images/svg/utilities/logoNET.svg';
 import ingranaggio from '../../assets/images/svg/utilities/ingranaggio.svg';
-
-const img_1 = '../../assets/images/svg/utilities/pancakeWritten.svg';
-
 const linksString = JSON.stringify(data);
+import { useState } from 'react';
+
+
 const links = JSON.parse(linksString).links;
 
 type Link = {
@@ -17,37 +17,52 @@ type Link = {
   href: string;
 };
 
-const Nav: React.FC<{}> = () => {
+const Nav: React.FC = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleDropdownHover = () => {
+    console.log(open);
+    setOpen(true);
+  };
   return (
-    <nav className={styles.navbar}>
-      <div className={styles['logo-container']}>
-        <img src={img_1} alt='pancakelogo' />
-        <div className={styles['links-container']}>
+    <nav className='navbar'>
+      <div className='logo-container'>
+        <img src={pancakeWritten} alt='pancakelogo' />
+        <div className='links-container'>
           {links.map((link: Link) => {
             return (
-              <ul key={link.href} className={styles['links']}>
-                <div className="nav-li"></div>
-                <li className={styles['nav-li']}>
-                  <Link key={link.href} to={link.href}>
-                    {link.label}
-                  </Link>
-                </li>
+              <ul key={link.href} className='links'>
+                <div className='nav-li'>
+                  <li
+                    onMouseOver={() => handleDropdownHover}
+                    className='nav-li'
+                  >
+                    <Link key={link.href} to={link.href}>
+                      {link.label}
+                    </Link>
+                  </li>
+                  {open && (
+                    <ul>
+                      <li>Item 1</li>
+                      <li>Item 2</li>
+                      <li>Item 3</li>
+                      <li>Item 4</li>
+                    </ul>
+                  )}
+                </div>
               </ul>
             );
           })}
         </div>
       </div>
-      <div className={styles['interactionsContainer']}>
-        
-        <img className={styles['growing']} src={logoPancake} alt='logo Pancake' />
-        <button className={styles['transparent'] }>
-          <img className={styles['tra']} src={logoNET} alt='logo net' />
+      <div className='interactionsContainer'>
+        <img className='growing' src={logoPancake} alt='logo Pancake' />
+        <button className='transparent'>
+          <img className='tra' src={logoNET} alt='logo net' />
         </button>
-        <img className={styles['tra']} src={ingranaggio} alt='logo' />
+        <img className='tra' src={ingranaggio} alt='logo' />
 
-        <button className={styles['connectWalletButton']}>
-          Connect Wallet
-        </button>
+        <button className='connectWalletButton'>Connect Wallet</button>
       </div>
     </nav>
   );

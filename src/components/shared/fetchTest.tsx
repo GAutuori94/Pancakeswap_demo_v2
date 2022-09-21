481f4020668b69875d5c6373111cd722482ed828
Alessio Ilardi <alessio.ilardi@gmail.com>
2022-09-21 21:35:58 +0200

    Chore: format some code after merge

--- src/components/shared/fetchTest.tsx
+++ src/components/shared/fetchTest.tsx
@@ -3,36 +3,32 @@ import React from "react";
 import { ExchangeInfoSymbol } from "../../types";
 import { useSymbols } from "../context/symbolsContext";
 
-
-export function ResponseRender(){
-    return <SymbolsRender />
+export function ResponseRender() {
+  return <SymbolsRender />;
 }
 
 export default function SymbolsRender() {
+  const { dataFetchedLoading, dataFetchedError, dataFetched } = useSymbols();
 
-    const {dataFetchedLoading, dataFetchedError, dataFetched} = useSymbols()
-
-
-    return (
-        <div className="symbols_container">
-            {(dataFetchedLoading) && <h1>Loading ...</h1>}
-            {(dataFetchedError) && <h1>Errore nel fetch ...</h1>}
-            {dataFetched.symbols?.map((item) => <SymbolsItem item={item}/>)}
-        </div>
-    )
+  return (
+    <div className="symbols_container">
+      {dataFetchedLoading && <h1>Loading ...</h1>}
+      {dataFetchedError && <h1>Errore nel fetch ...</h1>}
+      {dataFetched.symbols?.map((item) => (
+        <SymbolsItem item={item} />
+      ))}
+    </div>
+  );
 }
 
-
-
-export function SymbolsItem ({item}: {item: ExchangeInfoSymbol}) {
-
-    return (
-        <>
-            <div>
-                <h1>{item.symbol}</h1>
-                <h1>{item.baseAsset}</h1>
-                <h1>{item.quoteAsset}</h1>
-            </div>
-        </>
-    )
+export function SymbolsItem({ item }: { item: ExchangeInfoSymbol }) {
+  return (
+    <>
+      <div>
+        <h1>{item.symbol}</h1>
+        <h1>{item.baseAsset}</h1>
+        <h1>{item.quoteAsset}</h1>
+      </div>
+    </>
+  );
 }

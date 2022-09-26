6729f10e8502b79927a562d14f31550b4671ae6d
Alessio Ilardi <alessio.ilardi@gmail.com>
2022-09-26 22:16:03 +0200

    👌 IMPROVE: add custom colors to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        cardBorder: "#e7e3eb",
+        text: "#280d5f",
+        textSubtle: "#7a6eaa",
+      },
       spacing: {
         128: "32rem",
         144: "36rem",

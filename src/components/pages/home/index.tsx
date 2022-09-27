c775dfece2ae6ab72a278545e890ff5093926383
Alessio Ilardi <alessio.ilardi@gmail.com>
2022-09-27 14:49:13 +0200

    Chore: remove useless line

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -3,10 +3,5 @@ import TopCardSection from "./top-card-section";
 import "../../../style/css/index.css";
 
 export default function Home() {
-  return (
-    <>
-      <h1>This is the homepage!</h1>
-      <TopCardSection />
-    </>
-  );
+  return <TopCardSection />;
 }

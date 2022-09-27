1da8e63518d6d8d89eb17d8e06a3e5ed851cfabd
Alessio Ilardi <alessio.ilardi@gmail.com>
2022-09-27 13:07:34 +0200

    📦 NEW: add components for top card section [⚓TODO]

--- src/components/pages/home/top-card-section.tsx
+++ src/components/pages/home/top-card-section.tsx
@@ -1,70 +1,107 @@
 import React from "react";
+import { UsersIcon, TradesIcon, StackedIcon } from "./card-icons";
 
 import purpleBunnyIcon from "../../../assets/images/svg/icons/purpleBunnyIcon.svg";
-import usersIcon from "../../../assets/images/svg/icons/users.svg";
-import tradesIcon from "../../../assets/images/svg/icons/trades.svg";
-import stackedIcon from "../../../assets/images/svg/icons/stacked.svg";
+
+interface TopCardProps {
+  children: JSX.Element;
+  color?: string;
+  blackText: string;
+  coloredText: string;
+  subtleText: string;
+}
+
+/* TODO: there needs to be a way to style these cards conditionally,
+  either with props or with custom styling rules */
+
+function TopCard({
+  children,
+  blackText,
+  coloredText,
+  subtleText,
+}: TopCardProps) {
+  return (
+    <div
+      id="card-border"
+      className="bg-cardBorder rounded-3xl overflow-hidden relative mr-4 mb-0 h-fit px-px pb-1"
+    >
+      <div id="inner-box" className="w-full h-full bg-white rounded-3xl">
+        <div id="card-body" className="p-6">
+          <div id="icon-container" className="absolute top-6 right-6">
+            {children}
+          </div>
+          <div
+            id="card-text"
+            className="min-w-[232px] min-h-[168px] w-fit mt-16 flex flex-col box justify-end"
+          >
+            <h2 className="text-[40px] leading-[1.1] font-semibold">
+              {blackText}
+            </h2>
+            <h2 className="mb-6 font-semibold leading-[1.1] text-[40px] text-secondary">
+              {coloredText}
+            </h2>
+            <div
+              id="card-footer"
+              className="text-[1rem] leading-normal font-normal text-textSubtle"
+            >
+              {subtleText}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default function TopCardSection() {
   return (
-    <section id="top_card_section">
-      <div className="top-cards-background">
+    <section id="top-card-section">
+      <div
+        id="top-cards-background"
+        className="bg-gradient-to-b from-[#fff] to-[#d7caec] py-12 flex flex-col items-center z-10 relative"
+      >
         <img alt="purple bunny icon" src={purpleBunnyIcon} />
-        <div className="card-section-text">
-          <h2>Used by millions.</h2>
-          <h2>Trusted with billions.</h2>
-          <p>
+        <div id="card-section-text" className="text-center">
+          <h2 className="font-semibold text-4xl">Used by millions.</h2>
+          <h2 className="font-semibold text-4xl mb-8">
+            Trusted with billions.
+          </h2>
+          <p className="font-normal text-textSubtle">
             PancakeSwap has the most users of any decentralized platform, ever.
           </p>
-          <p>
+          <p className="font-normal text-textSubtle mb-5">
             And those users are now entrusting the platform with over $3.6
             billion in funds.
           </p>
-          <p>Will you join them?</p>
+          <p className="font-semibold text-textSubtle mb-8">
+            Will you join them?
+          </p>
         </div>
-        <div className="top-cards-container">
-          <div className="card-element">
-            <div className="inner-box">
-              <div className="card-body">
-                <div className="symbol-container">
-                  <img alt="users icon" src={usersIcon} />
-                </div>
-                <div className="card-text">
-                  <h2>1.9 million</h2>
-                  <h2>users</h2>
-                  <div className="card-footer">in the last 30 days</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card-element">
-            <div className="inner-box">
-              <div className="card-body">
-                <div className="symbol-container">
-                  <img alt="trades icon" src={tradesIcon} />
-                </div>
-                <div className="card-text">
-                  <h2>23 million</h2>
-                  <h2>trades</h2>
-                  <div className="card-footer">made in the last 30 days</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card-element">
-            <div className="inner-box">
-              <div className="card-body">
-                <div className="symbol-container">
-                  <img alt="stacks icon" src={stackedIcon} />
-                </div>
-                <div className="card-text">
-                  <h2>$3.9 billion</h2>
-                  <h2>staked</h2>
-                  <div className="card-footer">Total Value Locked</div>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div id="top-cards-container" className="flex flex-row">
+          <TopCard
+            color="secondary"
+            blackText="1.9 million"
+            coloredText="users"
+            subtleText="in the last 30 days"
+          >
+            <UsersIcon />
+          </TopCard>
+          <TopCard
+            color="primary"
+            blackText="23 million"
+            coloredText="trades"
+            subtleText="made in the last 30 days"
+          >
+            <TradesIcon />
+          </TopCard>
+          <TopCard
+            color="failure"
+            blackText="$4 billion"
+            coloredText="staked"
+            subtleText="Total Value Locked"
+          >
+            <StackedIcon />
+          </TopCard>
         </div>
       </div>
     </section>

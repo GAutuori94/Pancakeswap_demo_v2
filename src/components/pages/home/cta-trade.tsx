3e7367721af547e3613972585c03b250ced78f8e
Pietro Oss <pietrooss92@gmail.com>
2022-09-28 09:04:01 +0200

    fix: mb and mx of cutoff

--- src/components/pages/home/cta-trade.tsx
+++ src/components/pages/home/cta-trade.tsx
@@ -6,23 +6,23 @@ export default function CtaTrade() {
   return (
     <section id="flex flex-col">
       <div className="container mx-auto flex flex-row max-w-screen-lg ">
-        <div className="flex flex-col flex-1 self-center mt justify-center h-screen ">
-          <h2 className="text-4xl font-bold m-4">
+        <div className="flex flex-col flex-1 self-center justify-center h-screen ">
+          <h2 className="text-4xl font-bold mb-6">
             <span className="text-purple-600">Trade</span> anything. No
             registration, no hassle.
           </h2>
           <div>
-            <p className=" text-gray-500 text-lg font-bold">
+            <p className=" text-gray-500 text-lg font-bold mb-6">
               Trade any token on BNB Smart Chain in seconds, just by connecting
               your wallet.
             </p>
           </div>
-          <div className="flex flex-row self-start mx-10">
-            <button className="rounded-lg bg-green-400  hover:opacity-60 text-white font-bold py-2 px-4 mx-4">
+          <div className="flex flex-row ">
+            <button className="rounded-lg bg-green-400  hover:opacity-60 text-white font-bold py-2 px-4 ">
               Trade Now
             </button>
             <a
-              className="text-green-400 rounded-lg font-bold hover:underline decoration-2 py-1 px-4 mx-4"
+              className="text-green-400 rounded-lg font-bold hover:underline decoration-2 py-1 px-4 "
               href=""
               target="_blank"
             >

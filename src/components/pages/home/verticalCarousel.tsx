9a244bd7c50ea3b8d8d1db5ddc725992663f7e46
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-10-10 15:24:28 +0200

    Vertical carousel working

--- src/components/pages/home/verticalCarousel.tsx
+++ src/components/pages/home/verticalCarousel.tsx
@@ -10,43 +10,20 @@ export function VerticalCarousel() {
   const {dataFetchedLoading: parsedDataFetchedLoading, dataFetchedError: parsedDataFetchedError, parsedData} = useKline();
   const {dataFetchedLoading: tickerDataFetchedLoading, dataFetchedError: tickerDataFetchedError, dataFetched: tickerDataFetched} = useTicker();
 
-  const [showGrid1, setShowGrid1] = useState(false);
-  const [showGrid2, setShowGrid2] = useState(true);
+  const [showGrid, setShowGrid] = useState(0);
 
+  const viewNumber = 1;
+
+  const handleShowGrid = () => setShowGrid(showGrid >= viewNumber ? 0 : showGrid +1);
 
  useEffect(() => {
-    const gridInterval1 = setInterval(() => {
-      setShowGrid1(true);
-      const gridTimeOut1 = setTimeout(() => {
-        setShowGrid1(false)
-      }, 4000)
-      return () => clearTimeout(gridTimeOut1);
+    const gridInterval = setInterval(() => {
+      handleShowGrid();
     }, 4000);
-    return () => {clearInterval(gridInterval1)};
-  }, [showGrid1]);
+    return () => {clearInterval(gridInterval)};
+  }, [showGrid]);
 
-  useEffect(() => {
-    const gridInterval2 = setInterval(() => {
-      setShowGrid2(false);
-      const gridTimeOut2 = setTimeout(() => {
-        setShowGrid2(true)
-      }, 4000)
-      return () => clearTimeout(gridTimeOut2);
-    }, 4000);
-    return () => {clearInterval(gridInterval2)};
-  }, [showGrid2])
-
-
-  function handleGridChange() {
-    if(showGrid1 && !showGrid2 && dataFetched === true) {
-      setShowGrid1(false)
-      setShowGrid2(true)
-    } else
-    setShowGrid1(true)
-    setShowGrid2(false)
-  }
-
-
+  
   return (
 <div>
     <div className="vertical_carousel w-50 ml-10">
@@ -54,9 +31,9 @@ export function VerticalCarousel() {
         <div className="top__carousel_container__vertical_carousel mb-[24px] flex items-center">
           <h2 className="text-lightText text-fontSizeParagraph leading-lineHeightPar font-fontSizeParagraph font-fontHeavyWeight flex gap-0.5">
             Top
-            {showGrid1? <span className="text-lightSecondary dark:text-darkSecondary">Farms</span> : <span className="text-lightSecondary dark:text-darkSecondary">Syrup Pools</span>}
+            {showGrid === 0 ? <span className="text-lightSecondary dark:text-darkSecondary">Farms</span> : <span className="text-lightSecondary dark:text-darkSecondary">Syrup Pools</span>}
           </h2>
-          <button onClick={handleGridChange}>
+          <button onClick={handleShowGrid}>
             <svg
 
               viewBox="0 0 24 25"
@@ -73,7 +50,7 @@ export function VerticalCarousel() {
       </div>
 
 
-    <div className={showGrid2 ? "verticalCarousel_gridContainer h-[80px] relative" : "hidden"}>
+    <div className={showGrid === 1 ? "verticalCarousel_gridContainer h-[80px] relative" : "hidden"}>
       <div className="gridView__bottom__carousel_container grid grid-cols-5 gap-x-[70px]">
           {dataFetched.symbols?.slice(0, 5).map((item) =>
           <div className="text-lightSecondary dark:text-darkSecondary mb-[8px] pr-[16px] " key={item.symbol}> {item.symbol} </div>)}
@@ -83,7 +60,7 @@ export function VerticalCarousel() {
         </div>
       </div>
 
-    <div className={showGrid1? "verticalCarousel_gridContainer h-[80px] relative" : "hidden"}>
+    <div className={showGrid === 0? "verticalCarousel_gridContainer h-[80px] relative" : "hidden"}>
       <div className="gridView__bottom__carousel_container grid grid-cols-5 gap-x-[70px]">
           {dataFetched.symbols?.slice(5, 10).map((item) =>
           <div className="text-lightSecondary dark:text-darkSecondary mb-[8px] pr-[16px] " key={item.symbol}> {item.symbol} </div>)}

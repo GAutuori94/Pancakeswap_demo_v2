c4538fde67c7646134ed342458167d68da1adb20
Pietro Oss <pietrooss92@gmail.com>
2022-10-10 16:12:54 +0200

    add banner style

--- src/components/pages/home/BannerHero.tsx
+++ src/components/pages/home/BannerHero.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { FullButton } from "../../shared/buttons";
-
 import "../../../style/index.css";
 import "../../../style/bannerHero.css";
 import bannerLottery from "../../../assets/images/png/lotteryBanner.png";
@@ -10,16 +9,18 @@ import board from "../../../assets/images/png/bannerBord.png";
 function BannerHeroLeft() {
   return (
     <>
-      <div className="relative h-fit flex flex-row p-5">
-        <div className=" flex flex-col justify-between">
-          <h2 className="text-base color-(var[#ED4B9E]) ">Win 11000$</h2>
-          <img src={board} alt="board" />
-          <h2 className="text-xl font-bold text-[lightGradientGold]">time</h2>
+      <div className="banner_hero__perpetual_container ">
+        <div className="left__perpetual_container p-5">
+          <h2 className=" text-xl text-gold">Perpetual Futures</h2>
+          <h2 className="text-xl text-white drop-shadow-lg font-bold colors-gold">
+            UP TO 100X LEVERAGE
+          </h2>
           <FullButton />
         </div>
-        <span className="">
-          <img className="w-full" src={bannerLottery} />
-        </span>
+
+        <div className="banner_right">
+          <img className="w-80 mb-2" src={bannerPerpetual} />
+        </div>
       </div>
     </>
   );
@@ -28,18 +29,20 @@ function BannerHeroLeft() {
 function BannerHeroRight() {
   return (
     <>
-      <div className="relative flex flex-row p-5 ">
-        <div className=" flex flex-col justify-between">
-          <h2 className=" text-xl text-gold">Perpetual Futures</h2>
-          <h2 className="text-xl drop-shadow-lg border-darkSecondary font-bold colors-gold">
-            UP TO 100X LEVERAGE
+      <div className="banner_hero__lottery_container">
+        <div className="left__lottery_container p-5">
+          <h1 className="text-xl font-bold text-gold">time</h1>
+          <h2 className="text-md text-white drop-shadow-lg font-bold">
+            Win $110.720 in Lottery
           </h2>
+          <img className="absolute" src={board} alt="board" />
           <FullButton />
         </div>
-
-        <span className="">
-          <img className="w-80" src={bannerPerpetual} />
-        </span>
+        <div>
+          <span className="banner_right">
+            <img className="mb-6" src={bannerLottery} />
+          </span>
+        </div>
       </div>
     </>
   );
@@ -49,22 +52,26 @@ export function BannerHero() {
   const [isShown, setIsShown] = useState("rightBanner");
   return (
     <>
-      <div className="bg-[#7645D9] darkGradientViolet m-auto w-2/4 rounded-3xl mt-10 ">
-        {isShown === "leftBanner" && <BannerHeroLeft />}
-        {isShown === "rightBanner" && <BannerHeroRight />}
-        {/* classe per switch */}
-        <div className="flex flex-row justify-center pb-4">
-          <input
-            type="button"
-            onClick={() => setIsShown("rightBanner")}
-            className="bg-white cursor-pointer rounded-l-lg w-10 h-3"
-          />
+      <div className="banner_hero__main_container">
+        <div className="banner_hero__wrapper">
+          <div className="banner_hero__inner_container">
+            {isShown === "leftBanner" && <BannerHeroLeft />}
+            {isShown === "rightBanner" && <BannerHeroRight />}
 
-          <input
-            type="button"
-            onClick={() => setIsShown("leftBanner")}
-            className="bg-white cursor-pointer rounded-r-lg w-10 h-3"
-          />
+            <div className="flex flex-row justify-center h-100">
+              <input
+                type="button"
+                onClick={() => setIsShown("rightBanner")}
+                className="bg-white cursor-pointer rounded-l-lg w-10 h-2"
+              />
+
+              <input
+                type="button"
+                onClick={() => setIsShown("leftBanner")}
+                className="bg-white cursor-pointer rounded-r-lg w-10 h-2"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>

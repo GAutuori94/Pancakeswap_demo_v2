4713de651991011e3713ef373423c74c95591aaa
Pietro Oss <pietrooss92@gmail.com>
2022-10-10 16:45:23 +0200

    add style

--- src/components/pages/home/ctaExplore.tsx
+++ src/components/pages/home/ctaExplore.tsx
@@ -7,18 +7,20 @@ export default function CtaExplore() {
   return (
     <section id="flex flex-col">
       <div className="container mx-auto flex flex-row max-w-screen-lg ">
-        <div className="central-container">
+        <div className="flex flex-col flex-1 self-center justify-center h-screen ">
           <div className="flex flex-row flex-1 items-center justify-between place-content-start relative py-10 h-screen">
             <ImageComponent />
           </div>
-          <div className="side-text">
+          <div className="text-4xl font-bold mb-6">
             <h2>
               <span className="text-purple-600">Earn</span> passive income with
               crypto.
             </h2>
-            <p>PancakeSwap makes it easy to make your crypto work for you.</p>
+            <p className="text-gray-500 text-lg font-bold mb-6">
+              PancakeSwap makes it easy to make your crypto work for you.
+            </p>
           </div>
-          <div className="buttons">
+          <div className="flex flex-row gap-4">
             <FullButton />
             <OnlyBorderButton />
           </div>

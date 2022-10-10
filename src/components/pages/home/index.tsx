e31617a8a9a152ed1eedd32be506da0a68781b43
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-10-11 01:20:05 +0200

    Feat/settings_modal merged

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Footer from '../../shared/Footer'
 import Nav from '../../shared/Nav'
 
 export function Home () {
     return (
+        <>
         <Nav />
+        <Footer />
+        </>
     )
 }

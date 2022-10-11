6e31a920dfc019c11dfcc2e5a56e7b7a8fe47fee
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-10-11 13:38:39 +0200

    Fixing feat/cta_trade after merging feat/home_page

--- src/components/pages/home/ctaTrade.tsx
+++ src/components/pages/home/ctaTrade.tsx
@@ -5,16 +5,16 @@ import ImageComponentCoinTrade from "./ImageComponentCoinTrade";
 
 export default function CtaTrade() {
   return (
-    <section className="dark:bg-darkBackground w-100">
-      <div className="cta_trade_container flex items-center max-w-screen-lg ">
-        <div className="flex flex-col flex-1 self-center justify-center h-screen ">
+    <section className="dark:bg-darkBackground w-full flex justify-center ">
+      <div className=" flex items-center w-[50%] h-[700px]">
+        <div className="flex flex-col flex-1 self-center justify-center ">
           <h2 className="text-fontSizeSubheader font-fontHeavyWeight leading-[1.1] mb-6 dark:text-darkText">
-            <span className="text-lightSecondary">Trade</span> anything. No
+            <span className="text-lightSecondary">Trade   </span> anything. No <br />
             registration, no hassle.
           </h2>
           <div>
             <p className=" text-lightTextSubtle mb-6">
-              Trade any token on BNB Smart Chain in seconds, just by connecting
+              Trade any token on BNB Smart Chain in seconds, just by <br/> connecting
               your wallet.
             </p>
           </div>
@@ -23,7 +23,7 @@ export default function CtaTrade() {
             <VoidButton value="Learn" />
           </div>
         </div>
-        <div className="flex flex-row flex-1 items-center justify-between place-content-start relative py-10 h-screen">
+        <div className="flex flex-row flex-1 items-center justify-b etween place-content-start relative py-10">
           <ImageComponentCoinTrade />
         </div>
       </div>
